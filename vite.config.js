f379e6c7a84b348bd267370091a22c8f2ef61164
malagege <s9714020@gmail.com>
2021-08-30 22:49:16 +0800

    add icon

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,13 @@ import vue from '@vitejs/plugin-vue'
 import path from 'path'
 import { fileURLToPath } from 'url'
 import { dirname,join,resolve } from 'path'
+import Icons from 'unplugin-icons/vite'
 // const __filename = fileURLToPath(import.meta.url)
 // const __dirname = dirname(__filename)
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(),Icons({ /* options */ }),],
   resolve: {
     alias: {
       '@': `${join(__dirname, 'src')}`
