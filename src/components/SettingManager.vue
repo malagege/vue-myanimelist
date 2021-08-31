eb40ab1b75e4c8911773f2dc9c99dcb93acaa9ab
malagege <s9714020@gmail.com>
2021-08-31 23:16:06 +0800

    設定Modal初版畫面調整

--- src/components/SettingManager.vue
+++ src/components/SettingManager.vue
@@ -6,14 +6,47 @@
     <div class="modal-dialog modal-lg">
         <div class="modal-content">
             <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalToggleLabel">Modal 1</h5>
+        <h5 class="modal-title" id="exampleModalToggleLabel">設定</h5>
         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
         </div>
         <div class="modal-body">
-            Show a second modal and hide this one with the button below.
+            <div class="header d-flex">
+                <div class="flex-grow-1">
+                    <input class="form-input" type="text" placeholder="快速搜尋" v-model="searchText">
+                    <button type="button" class="btn btn-light" @click="searchText =''">清空</button>
+                </div>
+                <button class="btn btn-primary">另存設定</button>
+            </div>
+            <hr>
+            <div class="d-flex item">
+                <span class="item-title">
+                    title
+                </span>
+                <div>
+                    <button class="btn btn-sm btn-secondary">
+                        編輯名稱
+                    </button>
+                </div>
+                <div>
+                    <button class="btn btn-sm btn-info">
+                        讀取設定
+                    </button>
+                </div>
+                <div>
+                    <button class="btn btn-sm btn-success">
+                        覆蓋設定
+                    </button>
+                </div>
+                <div>
+                    <button class="btn btn-sm btn-danger">
+                        <IconTrashSharp></IconTrashSharp> 
+                    </button>
+                </div>
+
+            </div>
         </div>
         <div class="modal-footer">
-            <button class="btn btn-primary" data-bs-target="#exampleModalToggle2" data-bs-toggle="modal" data-bs-dismiss="modal">Open second modal</button>
+            <!-- <button class="btn btn-primary" data-bs-target="#exampleModalToggle2" data-bs-toggle="modal" data-bs-dismiss="modal">Open second modal</button> -->
         </div>
         </div>
     </div>
@@ -22,9 +55,24 @@
 </template>
 <script>
 import IconSetting from '~icons/icon-park-outline/setting-two'
+import IconTrashSharp from '~icons/ion/trash-sharp'
+
 export default {
+    props:{
+        settingVar: String,
+        storageName:{
+            type: String,
+            default: 'saveItems'
+        }
+    },
+    data(){
+        return {
+            searchText: ''
+        }
+    },
     components:{
-        IconSetting
+        IconSetting,
+        IconTrashSharp
     },
     methods:{
         alertHello(){
@@ -35,5 +83,17 @@ export default {
 
 </script>
 <style scoped>
+.header{
+    display: flex;
+}
+.item-title{
+    flex: 1;
 
+}
+.item>*{
+    padding: 12px 3px;
+}
+.item:hover{
+    background: lightgray;
+}
 </style>
