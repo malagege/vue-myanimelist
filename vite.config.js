c779c03d4b49195a8772562a51ad3162b9527314
malagege <s9714020@gmail.com>
2021-09-03 22:37:31 +0800

    github 環境變數

--- vite.config.js
+++ vite.config.js
@@ -4,15 +4,21 @@ import path from 'path'
 import { fileURLToPath } from 'url'
 import { dirname,join,resolve } from 'path'
 import Icons from 'unplugin-icons/vite'
+import { loadEnv } from 'vite'
 // const __filename = fileURLToPath(import.meta.url)
 // const __dirname = dirname(__filename)
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue(),Icons({ /* options */ }),],
-  resolve: {
-    alias: {
-      '@': `${join(__dirname, 'src')}`
+
+// 設定 Vite 環境變數方法
+// https://www.jianshu.com/p/4973bd983e96
+export default ({ mode }) => {
+  return defineConfig({
+    base:loadEnv(mode, process.cwd()).VITE_BASE,
+    plugins: [vue(),Icons({ /* options */ }),],
+    resolve: {
+      alias: {
+        '@': `${join(__dirname, 'src')}`
+      }
     }
-  }
-})
+  })
+}
