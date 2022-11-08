dd66143b9c74626a3ee2e02c1d3a30c94e7cdd33
malagege <s9714020@gmail.com>
2022-11-08 20:34:59 +0800

    標題字型調整

--- src/components/MyAnimeListItem.vue
+++ src/components/MyAnimeListItem.vue
@@ -6,7 +6,7 @@
         :data-order="item.order"
     >
         <div class="cancel" :class="{hidden: item.show}"></div>
-        <div class="myAnimeListItem__name">{{item.name}}</div>
+        <div class="myAnimeListItem__name" :class="{ fontSmall: stringBytes(item?.name) >= 22 }"><div class="myAnimeListItem__nameText">{{item.name}}</div></div>
         <div class="flex">
             <img class="myAnimeListItem__animeImg" :src="item.img" referrerpolicy="no-referrer" >
             <pre class="myAnimeListItem__staff">
@@ -32,6 +32,19 @@ export default {
             this.item.order = prompt(`請輸入「${this.item.name}」動畫名次`)
             event.preventDefault(); 
             this.$emit('update:item', this.item)
+        },
+        // 中文長度 2Byte 參考:https://www.puritys.me/docs-blog/article-107-String-Length-%E4%B8%AD%E6%96%87%E5%AD%97%E4%B8%B2%E9%95%B7%E5%BA%A6.html
+        stringBytes(c){
+            var n = c.length,s;
+            var len = 0;
+            for(var i = 0; i <n;i++){
+                s = c.charCodeAt(i);
+                while( s > 0 ){
+                    len++;
+                    s = s >> 8;
+                }
+            }
+            return len;
         }
     }
 }
@@ -81,10 +94,21 @@ export default {
 .myAnimeListItem__name{
     background: black;
     color: white;
-    padding-top: 3%;
+    display: grid;
+    justify-content: center;
+    align-items: center;
     height: 50px;
-    text-align: center;
+    /* text-align: center; */
     font-size: 1.4em;
+}
+
+.myAnimeListItem__nameText{
+    padding: 0px 8px;
+}
+
+
+.myAnimeListItem__name.fontSmall{
+    font-size: 1.1em;
 }
 
 .myAnimeListItem__animeImg{
