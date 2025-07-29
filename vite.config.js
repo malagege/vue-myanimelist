13d91fa4d20059d66888631cac9bf69d9d73d625
malagege <s9714020@gmail.com>
2025-07-29 23:10:15 +0800

    重構 Vite 配置，使用環境變數設置基礎路徑

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,16 @@
-const { defineConfig } = require('vite')
+const { defineConfig, loadEnv } = require('vite')
 const vue = require('@vitejs/plugin-vue')
 const path = require('path')
 const Icons = require('unplugin-icons/vite')
 
-module.exports = defineConfig({
-  base: '.',
+
+
+module.exports = defineConfig((mode) => ({
+  base: loadEnv(mode, process.cwd()).VITE_BASE,
   plugins: [vue(), Icons()],
   resolve: {
     alias: {
       '@': path.join(__dirname, 'src')
     }
   }
-})
+}))
